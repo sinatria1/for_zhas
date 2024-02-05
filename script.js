55c7375725be7dcf79d11018c28b821bea54868a
sinatria1 <103710578+sinatria1@users.noreply.github.com>
2024-02-05 10:38:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const mdiv = document.getElementById('message');
+const nbtn = document.querySelector('.no');
+const ybtn = document.querySelector('.yes');
+
+nbtn.addEventListener('click', ()=>{
+    const sw = window.innerWidth;
+    const sh = window.innerHeight;
+
+    const bw = nbtn.offsetWidth;
+    const bh = nbtn.offsetHeight;
+
+    const randx = Math.floor(Math.random() * (sw - bw));
+    const randy = Math.floor(Math.random() * (sh - bh));
+
+    nbtn.style.left = randx + 'px';
+    nbtn.style.top = randy + 'px';
+
+});
+
+ybtn.addEventListener('click', ()=>{
+    mdiv.textContent = "Woahhh i love you too zhas";
+});
