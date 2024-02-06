5342cd1babc9169415ee0fd20a54ec73bac182e5
sinatria1 <103710578+sinatria1@users.noreply.github.com>
2024-02-06 07:25:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,128 @@
-const mdiv = document.getElementById('message');
-const nbtn = document.querySelector('.no');
-const ybtn = document.querySelector('.yes');
+const question = [
+    {
+    question : 'Who does si think the prettiest girl in the world?',
+    answer : [
+        { text: 'Alaina Castillo', correct: false},
+        { text: 'Dua Lipa', correct: false},
+        { text: 'Zhas', correct: true},
+        { text: 'Sofia Reyes', correct: false}
+    ]
+},
+{
+    question : 'Who does have the prettiest voice?',
+    answer : [
+        { text: 'Zhas', correct: true},
+        { text: 'Sofia Reyes', correct: false},
+        { text: 'Alaina Castillo', correct: false},
+        { text: 'Dua Lipa', correct: false}
+    ]
+},
+{
+    question : 'Will you give me some cute pics of you tonight?',
+    answer : [
+        { text: 'Mmmm, maybe', correct: false},
+        { text: 'Nah', correct: false},
+        { text: 'I guess so', correct: false},
+        { text: 'Yes !!!!', correct: true}
+    ]
+},
 
-nbtn.addEventListener('click', ()=>{
-    const sw = window.innerWidth;
-    const sh = window.innerHeight;
+{
+    question : 'Will you sing me song tonight?',
+    answer : [
+        { text: 'No i hate my voice', correct: false},
+        { text: 'Yes but you have to sing me song first', correct: false},
+        { text: 'Later, i am shy', correct: false},
+        { text: 'Yes, how many songs you wanna hear?', correct: true}
+    ]   
+}
 
-    const bw = nbtn.offsetWidth;
-    const bh = nbtn.offsetHeight;
+]
 
-    const randx = Math.floor(Math.random() * (sw - bw));
-    const randy = Math.floor(Math.random() * (sh - bh));
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-btn');
 
-    nbtn.style.left = randx + 'px';
-    nbtn.style.top = randy + 'px';
+let currentQuestionIndex = 0;
+let score = 0;
 
-});
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
 
-ybtn.addEventListener('click', ()=>{
-    mdiv.textContent = "Woahhh STFU !!! i love you too zhas";
-});
+function showQuestion() {
+    // Clear previous buttons
+    answerButton.innerHTML = '';
+
+    resetState();
+
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = 'none';
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add('incorrect');
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = 'true';
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${question.length}!!!... and dont forget to sing me song hehe!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < question.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener('click', () => {
+    if(currentQuestionIndex < question.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+
+startQuiz();
